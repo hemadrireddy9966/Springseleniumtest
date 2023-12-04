c8b035e576d750e85626ef1ee66f45153028b1ee
Hemadri Thokala <hemadri.t@vitra.ai>
2023-12-04 10:58:29 +0530

    test 04-12-2023

---
+++ WTseleniumAutomationProject/stored_code.java
@@ -0,0 +1,36 @@
+<script type="text/javascript" src="https://d1wrgmw2jqph3l.cloudfront.net/web-translate/on-the-fly.min.js"></script>
+<script>
+		document.addEventListener("DOMContentLoaded", function () {
+			new VitraOnTheFly({
+				api_key: "vitra.dYQrEk5D7OozNTmIqUVy_vXifWG6TW",
+				glossaryId: "c4cee486-801f-452b-8bed-f7e50e1e454f",
+				position: "center-right",
+				theme: "dark",
+			});
+		});
+</script>
+
+
+<script type="text/javascript" src="https://d1wrgmw2jqph3l.cloudfront.net/web-translate/staging-on-the-fly.min.js"></script>
+<a		document.addEventListener("DOMContentLoaded", function () {
+		new VitraOnTheFly({
+		api_key: "vitra.ypjSc2iqTggk5RhN1pk1vZ9ZTTJogN",
+		glossaryId: "04aad67a-f323-49cb-9215-8c427be9dc96",
+		position: "center-right",
+		theme: "dark",
+		});
+		});
+</</script>
+
+
+<script type="text/javascript" src="https://d1wrgmw2jqph3l.cloudfront.net/web-translate/staging-on-the-fly.min.js"></script>script>
+<script>
+		document.addEventListener("DOMContentLoaded", function () {
+				new VitraOnTheFly({
+				api_key: "vitra.ypjSc2iqTggk5RhN1pk1vZ9ZTTJogN",
+				glossaryId: "04aad67a-f323-49cb-9215-8c427be9dc96",
+				position: "center-right",
+				theme: "dark",
+				});
+				});
+</script>script></script>
